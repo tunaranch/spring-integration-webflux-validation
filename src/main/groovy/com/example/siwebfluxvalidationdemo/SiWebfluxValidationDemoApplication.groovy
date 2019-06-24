cbe8a9654a15a330c37cf0e3af455612b9393304
Haikal Saadh <h.saadh@qut.edu.au>
2019-06-24 19:19:15 +1000

    Create project  from s.s.o

---
+++ src/main/groovy/com/example/siwebfluxvalidationdemo/SiWebfluxValidationDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.siwebfluxvalidationdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SiWebfluxValidationDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SiWebfluxValidationDemoApplication, args)
+    }
+
+}
