9266b7529f12a06cbdf14ffb6a82b2be04bbbc00
Haikal Saadh <h.saadh@qut.edu.au>
2019-06-24 20:32:00 +1000

    Add validation to the processing dequence

--- src/main/groovy/com/example/siwebfluxvalidationdemo/ValidatingFlowConfiguration.groovy
+++ src/main/groovy/com/example/siwebfluxvalidationdemo/ValidatingFlowConfiguration.groovy
@@ -2,6 +2,7 @@ package com.example.siwebfluxvalidationdemo
 
 import groovy.transform.ToString
 import org.reactivestreams.Publisher
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.ResolvableType
@@ -14,15 +15,21 @@ import org.springframework.integration.dsl.IntegrationFlows
 import org.springframework.integration.webflux.dsl.WebFlux
 import org.springframework.messaging.Message
 import org.springframework.messaging.MessageChannel
-import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.validation.BeanPropertyBindingResult
+import org.springframework.validation.Errors
+import org.springframework.validation.Validator
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.server.ServerWebInputException
 import reactor.core.publisher.Flux
 
 import javax.validation.constraints.NotEmpty
 
 @EnableIntegration
-@EnableWebFlux
 @Configuration
 class ValidatingFlowConfiguration {
+
+    @Autowired
+    Validator validator
 
     @Bean
     Publisher<Message<String>> helloFlow() {
@@ -54,12 +61,22 @@ class ValidatingFlowConfiguration {
     )
     Flux<String> greetingHandler(Flux<HelloRequest> seq) {
         seq
+                .doOnNext { HelloRequest it -> validate(it) }
                 .log()
                 .map { "Hello, ${it.name}" as String }
+    }
+
+    void validate(HelloRequest request) {
+        Errors errors = new BeanPropertyBindingResult(request, "request")
+        validator.validate(request, errors);
+        if (errors.hasErrors()) {
+            throw new ServerWebInputException(errors.toString());
+        }
     }
 }
 
 @ToString(includeNames = true)
+@Validated
 class HelloRequest {
 
     @NotEmpty
